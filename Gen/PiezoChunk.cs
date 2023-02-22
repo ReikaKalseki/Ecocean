d2ea940d858384f4c75d51dcef7cb901de483a14
ReikaKalseki <reikasminecraft@gmail.com>
2023-02-22 17:55:49 -0500

    Update 32

---
+++ Gen/PiezoChunk.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Handlers;
+using SMLHelper.V2.Crafting;
+
+using UnityEngine;
+using ReikaKalseki.DIAlterra;
+
+namespace ReikaKalseki.Ecocean
+{
+	public sealed class PiezoChunk : WorldGenerator {
+		
+		static PiezoChunk() {	
+		
+		}
+		
+		public PiezoChunk(Vector3 pos) : base(pos) {
+			
+		}
+		
+		public override void loadFromXML(XmlElement e) {
+			
+		}
+		
+		public override void saveToXML(XmlElement e) {
+			
+		}
+		
+		public override void generate(List<GameObject> generated) {
+			int n = UnityEngine.Random.Range(4, 7); //4-6 horizontal
+			Vector3[] angs = new Vector3[n+2];
+			for (int i = 0; i < n; i++) {
+				float ang = (360F/n)*i;
+				ang += UnityEngine.Random.Range(-15F, 15F);
+				angs[i] = new Vector3(UnityEngine.Random.Range(0, 360F), ang, UnityEngine.Random.Range(60F, 120F));
+			}
+			angs[angs.Length-1] = new Vector3(UnityEngine.Random.Range(-30F, 30F), UnityEngine.Random.Range(0, 360F), 0);
+			angs[angs.Length-2] = new Vector3(UnityEngine.Random.Range(150F, 210F), UnityEngine.Random.Range(0, 360F), 0);
+			foreach (Vector3 ang in angs) {
+				GameObject go = ObjectUtil.createWorldObject(EcoceanMod.piezo.ClassID);
+				go.transform.position = position;
+				go.transform.rotation = Quaternion.Euler(ang.x, ang.y, ang.z);//UnityEngine.Random.rotationUniform;
+				go.transform.position = go.transform.position+go.transform.up*UnityEngine.Random.Range(0.25F, 0.5F);
+				generated.Add(go);
+			}
+		}
+		
+		public override string ToString() {
+			return base.ToString();
+		}
+	}
+}
--- Objects/PiezoCrystal.cs
+++ Objects/PiezoCrystal.cs
@@ -23,6 +23,22 @@ namespace ReikaKalseki.Ecocean {
 	    }
 			
 	    public override GameObject GetGameObject() {
+						int n = UnityEngine.Random.Range(4, 7); //4-6 horizontal
+			Vector3[] angs = new Vector3[n+2];
+			for (int i = 0; i < n; i++) {
+				float ang = (360F/n)*i;
+				ang += UnityEngine.Random.Range(-15F, 15F);
+				angs[i] = new Vector3(UnityEngine.Random.Range(0, 360F), ang, UnityEngine.Random.Range(60F, 120F));
+			}
+			angs[angs.Length-1] = new Vector3(UnityEngine.Random.Range(-30F, 30F), UnityEngine.Random.Range(0, 360F), 0);
+			angs[angs.Length-2] = new Vector3(UnityEngine.Random.Range(150F, 210F), UnityEngine.Random.Range(0, 360F), 0);
+			foreach (Vector3 ang in angs) {
+				GameObject go = ObjectUtil.createWorldObject(EcoceanMod.piezo.ClassID);
+				go.transform.position = position;
+				go.transform.rotation = Quaternion.Euler(ang.x, ang.y, ang.z);//UnityEngine.Random.rotationUniform;
+				go.transform.position = go.transform.position+go.transform.up*UnityEngine.Random.Range(0.25F, 0.5F);
+				generated.Add(go);
+			}
 			GameObject world = ObjectUtil.createWorldObject(VanillaResources.DIAMOND.prefab);
 			world.EnsureComponent<TechTag>().type = TechType;
 			world.EnsureComponent<PrefabIdentifier>().ClassId = ClassID;
@@ -46,7 +62,7 @@ namespace ReikaKalseki.Ecocean {
 			Patch();
 			SNUtil.addPDAEntry(this, 8, "PlanetaryGeology", locale.pda, locale.getField<string>("header"));
 			ItemRegistry.instance.addItem(this);
-			//GenUtil.registerSlotWorldgen(ClassID, PrefabFileName, TechType, EntitySlot.Type.Creature, LargeWorldEntity.CellLevel.Far, BiomeType.CragField_OpenDeep_CreatureOnly, 1, 1F);
+			//GenUtil.registerSlotWorldgen(ClassID, PrefabFileName, TechType, EntitySlot.Type.Medium, LargeWorldEntity.CellLevel.VeryFar, BiomeType.UnderwaterIslands_IslandSides, 1, 1F);
 		}
 			
 	}
