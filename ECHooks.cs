132fcad56093a3e4d2f6e86d5634a09d82631a16
ReikaKalseki <reikasminecraft@gmail.com>
2023-10-27 21:53:21 -0400

    Update 89 - v39

--- ECHooks.cs
+++ ECHooks.cs
@@ -41,8 +41,7 @@ namespace ReikaKalseki.Ecocean {
 	    	DIHooks.onCyclopsTickEvent += tickCyclops;
 	    	
 	    	DIHooks.onEMPHitEvent += onEMPHit;
-	    	DIHooks.onEMPTouchEvent += onEMPTouch;
-	    	
+	    	DIHooks.onEMPTouchEvent += onEMPTouch;	    	
 	    	
 	    	DIHooks.getTemperatureEvent += getWaterTemperature;
 	    	
